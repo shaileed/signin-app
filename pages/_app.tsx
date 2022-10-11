59e8887fa44a7e7299f493ec9ebc910eaa21ca6b
shaileed <114909614+shaileed@users.noreply.github.com>
2022-10-11 18:06:51 +0530

    Revert "State managment"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import type { AppProps } from 'next/app'
-import {Provider} from 'react-redux';
-import store from '../store/store';
 import '../styles/globals.css'
-
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Provider store={store}><div className="mx-auto" style={{width: "200px;"}}> <Component {...pageProps} /></div></Provider>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
